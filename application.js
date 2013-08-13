629426c90428a4194bd54bcafa324b37b6d0d37d
Apprentice <apprentice@devbootcamp.com>
2013-08-13 16:31:11 -0700

    first commit

---
+++ application.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#style_editor").submit(function(event) {
+    event.preventDefault();
+    var selector = $("#selector").val();
+    var property = $("#property").val();
+    var value = $("#value").val();
+    $(selector).css(property, value);
+  });
+});
